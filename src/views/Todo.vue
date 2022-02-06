90e591d326247d547ad52de6297de7c9dfed89fa
Puddingz112 <bird.1457noppadon@gmail.com>
2022-02-06 23:49:07 +0700

    first commit

---
+++ src/views/Todo.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <Data @Input="validate"></Data>
+    <Table :desserts="desserts"></Table>
+  </v-container>
+</template>
+
+<script>
+import Data from "../components/Data.vue";
+import Table from "../components/Table.vue";
+export default {
+  data: () => ({
+    desserts: [],
+  }),
+  desserts: "Input",
+  methods: {
+    validate(select, name) {
+      this.desserts.push({
+        name: select + " : " + name,
+      });
+    },
+  },
+  components: { Data, Table },
+};
+</script>
+
+<style></style>
